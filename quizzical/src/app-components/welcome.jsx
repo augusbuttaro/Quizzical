20ea919019db72064451f7bd171a75d898247dd6
augusbuttaro <augusbuttaro@gmail.com>
2024-03-22 12:45:49 -0300

    Update welcome.jsx

--- quizzical/src/app-components/welcome.jsx
+++ quizzical/src/app-components/welcome.jsx
@@ -6,7 +6,7 @@ function Welcome(props) {
             <div className='welcome'>
                 <div className='welcome-inside'>
                 <h1>Quizzical</h1>
-                <p>Five questions await. How many can you nail? Let's find out!</p>
+                <p>Fiv questions await. How many can you nail? Let's find out!</p>
                 <button onClick={props.handleClick}>Start quiz</button>
                 </div>
             </div>
